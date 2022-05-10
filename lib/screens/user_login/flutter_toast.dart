fff9c34251d085f1d4d9ba4ecc06e6da71944395
Mohamed Ashry <am5a20109999@gmail.com>
2022-05-10 12:07:44 +0200

    Maintenance

--- lib/screens/user_login/flutter_toast.dart
+++ lib/screens/user_login/flutter_toast.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:rescue2/screens/colors.dart';
@@ -10,9 +11,23 @@ void showToast()=> Fluttertoast.showToast(
    // AppSettings.openLocationSettings();
 
 );
-void showToast2()=> Fluttertoast.showToast(
-  msg: "this email already exist",
+void showToast2(String msg)=> Fluttertoast.showToast(
+  msg: msg,
   fontSize: 18,
   backgroundColor: Mycolor.darkblue,
 
 );
+
+Future createData({required String coll ,required String coll2 , required String doc,required String data}) async {
+  final docMilage = FirebaseFirestore.instance.collection(coll).doc(doc).collection(coll2).doc(coll2);
+  final json = {
+    'Milage':data,
+    'Date':DateTime.now(),
+  };
+  await docMilage.set(json).then((value) {
+    showToast2("Done");
+
+  }).onError((error, stackTrace) {
+    showToast2(error.toString());
+  });
+}
