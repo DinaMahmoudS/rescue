4871a5ff90e47d96dec51fdf456bf0d6b120c6ca
Mohamed Ashry <am5a20109999@gmail.com>
2022-05-29 03:50:55 +0200

    finish report

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import'package:flutter/material.dart';
 import 'package:rescue2/screens/colors.dart';
 import 'package:rescue2/screens/navigation_bar.dart';
+import 'package:rescue2/screens/user_login/flutter_toast.dart';
 
 class Report extends StatefulWidget {
   const Report({Key? key}) : super(key: key);
@@ -10,6 +13,9 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+
+  late var _locationCon = TextEditingController();
+  late var causesCon = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,48 +53,86 @@ class _ReportState extends State<Report> {
       body:Padding(
         padding:const EdgeInsets.all(11.0),
         child:Form(
-       child:SingleChildScrollView(
-         child: Column(
-
-           children:<Widget>[
-             const SizedBox(height: 20),
-             Image.asset(
-               "assets/images/traffic.png",
-               width: 170,
-               height: 170,
-             ),
-             const SizedBox(height: 40),
-
-             TextField(
-               decoration:InputDecoration(
-                 labelText: "Location",
-                 labelStyle: TextStyle(
-                     fontSize: 25,
-                     color: Mycolor.darkblue,
-                     fontWeight: FontWeight.bold ),
-                 fillColor: Colors.black12,
-                 filled: true,
-                 border:OutlineInputBorder(borderRadius:BorderRadius.circular(20)),
-                 hintText:'region,main street name,branch street name',
-                   suffixIcon: Icon(Icons.send)
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(height: 20),
+                    Image.asset(
+                      "assets/images/traffic.png",
+                      width: 170,
+                      height: 170,
+                    ),
+                    const SizedBox(height: 40),
+                    TextFormField(
+                      controller: _locationCon,
+                      decoration: InputDecoration(
+                          labelText: "Location",
+                          labelStyle: TextStyle(
+                              fontSize: 25,
+                              color: Mycolor.darkblue,
+                              fontWeight: FontWeight.bold),
+                          fillColor: Colors.black12,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          hintText:
+                              'region,main street name,branch street name',
+                          suffixIcon: Icon(Icons.send)
                ),
              ),
              SizedBox(height: 30),
-                 TextField(
-                 decoration:InputDecoration(
-                   labelText: "What causes traffic congestion",
-                     labelStyle: TextStyle(
-                         fontSize: 20,
-                         color: Mycolor.darkblue,
-                         fontWeight: FontWeight.bold ),
-
-                   fillColor: Colors.black12,
-                 filled: true,
-                 border:OutlineInputBorder(borderRadius:BorderRadius.circular(20),),
-                 hintText:'ex accident',
+                TextFormField(
+                  controller: causesCon,
+                      decoration: InputDecoration(
+                          labelText: "What causes traffic congestion",
+                          labelStyle: TextStyle(
+                              fontSize: 20,
+                              color: Mycolor.darkblue,
+                              fontWeight: FontWeight.bold),
+                          fillColor: Colors.black12,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText:'ex accident',
                    suffixIcon: Icon(Icons.send)
           ),
           ),
+          SizedBox(height: 30),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Mycolor.red,
+                          fixedSize: const Size(250, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_locationCon.text.isEmpty) {
+                            showToast2("Please enter your location");
+                          }else if(causesCon.text.isEmpty){
+                            showToast2("Please enter your causes");
+                          }else{
+                            FirebaseFirestore.instance.collection('traffic').add({
+                              'location': _locationCon.text,
+                              'causes': causesCon.text,
+                              'uuid': '${FirebaseAuth.instance.currentUser!.uid}',
+                            }).whenComplete(() => {
+                              showToast2("Report sent successfully"),
+                            });
+
+
+                          }
+
+
+
+                        },
+                        child: const Text(
+                          'Report',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        )),
            ],
          ),
        ),
